ae021cd406b0141e0f55a185f56c1b01d482454c
chunglijung <andy28572463@gmail.com>
2024-05-11 20:30:54 +0800

    feat: 新增ubike ui

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,102 +1,123 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { initFlowbite } from 'flowbite'
+import { ref, onMounted } from 'vue';
+import { initFlowbite } from 'flowbite';
 
 // initialize components based on data attribute selectors
 onMounted(() => {
-  initFlowbite()
-})
+  initFlowbite();
+});
 
-const msg = ref('Hello, Vue 3!')
+/*
+"sno"：站點編號，這裡是 500101001。
+"sna"：站點名稱，中文為 YouBike2.0_捷運科技大樓站。
+"sarea"：站點所在區域，這裡位於大安區。
+"mday"：資料更新時間，是在 2024 年 5 月 11 日晚上 20:15:19 更新的。
+"ar"：站點地址，這裡是在復興南路二段235號前。
+"sareaen"：站點所在區域的英文名稱，是 Daan Dist.。
+"snaen"：站點名稱的英文翻譯，是 YouBike2.0_MRT Technology Bldg. Sta.。
+"aren"：站點地址的英文翻譯，是 No.235， Sec. 2， Fuxing S. Rd.。
+"act"：站點是否在使用中，這裡是 1 代表在使用中。
+"srcUpdateTime"：原始資料來源的更新時間，是在 2024 年 5 月 11 日晚上 20:15:26 更新的。
+"updateTime"：資料更新時間，是在 2024 年 5 月 11 日晚上 20:15:52。
+"infoTime"：資訊時間，是在 2024 年 5 月 11 日晚上 20:15:19。
+"infoDate"：資訊日期，是在 2024 年 5 月 11 日。
+"total"：總車位數量，這裡是 28 個。
+"available_rent_bikes"：可租借的腳踏車數量，這裡是 1 輛。
+"latitude"：站點的緯度坐標，這裡是 25.02605。
+"longitude"：站點的經度坐標，這裡是 121.5436。
+"available_return_bikes"：可歸還的腳踏車數量，這裡是 27 輛。
+*/
+const bikeinfo = ref([
+  {
+    sno: '500101001',
+    sna: 'YouBike2.0_捷運科技大樓站',
+    sarea: '大安區',
+    mday: '2024-05-11 20:15:19',
+    ar: '復興南路二段235號前',
+    sareaen: 'Daan Dist.',
+    snaen: 'YouBike2.0_MRT Technology Bldg. Sta.',
+    aren: 'No.235， Sec. 2， Fuxing S. Rd.',
+    act: '1',
+    srcUpdateTime: '2024-05-11 20:15:26',
+    updateTime: '2024-05-11 20:15:52',
+    infoTime: '2024-05-11 20:15:19',
+    infoDate: '2024-05-11',
+    total: 28,
+    available_rent_bikes: 1,
+    latitude: 25.02605,
+    longitude: 121.5436,
+    available_return_bikes: 27
+  }
+]);
 </script>
 
 <template>
-  <main>
-    <h1>{{ msg }}</h1>
-
-    <!-- Modal toggle -->
-    <button
-      data-modal-target="default-modal"
-      data-modal-toggle="default-modal"
-      class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      type="button"
+  <div class="container mx-auto px-4">
+    <!--top bar start-->
+    <nav
+      class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600"
     >
-      Toggle modal
-    </button>
-
-    <!-- Main modal -->
-    <div
-      id="default-modal"
-      tabindex="-1"
-      aria-hidden="true"
-      class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
-    >
-      <div class="relative p-4 w-full max-w-2xl max-h-full">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <!-- Modal header -->
-          <div
-            class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600"
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+            >YouBike範本</span
           >
-            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">Terms of Service</h3>
-            <button
-              type="button"
-              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              data-modal-hide="default-modal"
-            >
-              <svg
-                class="w-3 h-3"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 14"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                />
-              </svg>
-              <span class="sr-only">Close modal</span>
-            </button>
+        </a>
+        <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <div class="mr-2">
+            <input
+              type="text"
+              id="first_name"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeholder="輸入關鍵字查詢"
+              required
+            />
           </div>
-          <!-- Modal body -->
-          <div class="p-4 md:p-5 space-y-4">
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              With less than a month to go before the European Union enacts new consumer privacy
-              laws for its citizens, companies around the world are updating their terms of service
-              agreements to comply.
-            </p>
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on
-              May 25 and is meant to ensure a common set of data rights in the European Union. It
-              requires organizations to notify users as soon as possible of high-risk data breaches
-              that could personally affect them.
-            </p>
-          </div>
-          <!-- Modal footer -->
-          <div
-            class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600"
+          <button
+            type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center cursor-pointer"
           >
-            <button
-              data-modal-hide="default-modal"
-              type="button"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              I accept
-            </button>
-            <button
-              data-modal-hide="default-modal"
-              type="button"
-              class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-            >
-              Decline
-            </button>
-          </div>
+            查詢
+          </button>
         </div>
       </div>
+    </nav>
+    <!--top bar end-->
+    <!--datatable start-->
+    <div class="relative overflow-x-auto shadow-md top-20">
+      <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-white uppercase bg-blue-600 dark:text-white">
+          <tr>
+            <th class="font-semibold text-center p-2">站點編號</th>
+            <th class="font-semibold text-center p-2">站點名稱</th>
+            <th class="font-semibold text-center p-2">站點所在區域</th>
+            <th class="font-semibold text-center p-2">站點地址</th>
+            <th class="font-semibold text-center p-2">總車位數量</th>
+            <th class="font-semibold text-center p-2">可租借的腳踏車數量</th>
+            <th class="font-semibold text-center p-2">站點緯度</th>
+            <th class="font-semibold text-center p-2">站點經度</th>
+            <th class="font-semibold text-center p-2">可歸還的腳踏車數量</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 hover:bg-blue-100"
+          >
+            <td class="p-2 text-center">{{ bikeinfo[0].sno }}</td>
+            <td class="p-2 text-center">{{ bikeinfo[0].sna }}</td>
+            <td class="p-2 text-center">{{ bikeinfo[0].sarea }}</td>
+            <td class="p-2 text-center">{{ bikeinfo[0].ar }}</td>
+            <td class="p-2 text-center">{{ bikeinfo[0].total }}</td>
+            <td class="p-2 text-center">{{ bikeinfo[0].available_rent_bikes }}</td>
+            <td class="p-2 text-center">{{ bikeinfo[0].latitude }}</td>
+            <td class="p-2 text-center">{{ bikeinfo[0].longitude }}</td>
+            <td class="p-2 text-center">{{ bikeinfo[0].available_return_bikes }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  </main>
+
+    <!--datatable end-->
+  </div>
 </template>
