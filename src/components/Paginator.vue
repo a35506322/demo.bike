1bf9afc067e1a50037327691f8c33235878b8715
chunglijung <andy28572463@gmail.com>
2024-05-21 21:26:40 +0800

    refacotr: 新增分頁元件

---
+++ src/components/Paginator.vue
@@ -0,0 +1,96 @@
+<script setup>
+import { computed, ref, watch } from 'vue';
+
+const props = defineProps(['totalRecords', 'rows', 'isReset']);
+const emits = defineEmits(['page']);
+
+const rows = computed(() => (props.rows ? props.rows : 10));
+const totalRecords = computed(() => Math.ceil(props.totalRecords / rows.value));
+const isReset = computed(() => props.isReset);
+
+// 當前頁數
+const currentPage = ref(1);
+// 顯示頁數
+const maxPage = 10;
+
+// 計算當前顯示頁碼最多十頁
+const currentPageList = computed(() => {
+  let result = [];
+  // 取得當前頁碼的前五頁
+  const start = currentPage.value - 5 > 0 ? currentPage.value - 5 : 1;
+
+  // 取得當前頁碼的後五頁
+  const end = start + maxPage - 1 <= totalRecords.value ? start + maxPage - 1 : totalRecords.value;
+  for (let i = start; i <= end; i++) {
+    result.push(i);
+  }
+  return result;
+});
+
+// 切換頁數
+const changePage = (page) => {
+  currentPage.value = page;
+};
+
+// 前一頁
+const prevPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value -= 1;
+  }
+};
+
+// 下一頁
+const nextPage = () => {
+  if (currentPage.value < totalRecords.value) {
+    currentPage.value += 1;
+  }
+};
+
+watch(currentPage, (val) => {
+  emits('page', val);
+});
+
+watch(isReset, (val) => {
+  if (val) {
+    currentPage.value = 1;
+  }
+});
+</script>
+<template>
+  <nav aria-label="Page navigation example" class="text-center mt-3">
+    <ul class="inline-flex -space-x-px text-base h-10">
+      <li>
+        <a
+          href="#"
+          class="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700"
+          @click="prevPage"
+          :class="[currentPage === 1 ? 'cursor-not-allowed' : '']"
+          >上一頁</a
+        >
+      </li>
+      <li v-for="(item, index) in currentPageList" :key="item">
+        <a
+          :class="[
+            currentPage === item
+              ? 'bg-blue-600 text-white hover:bg-blue-700'
+              : 'text-gray-500 bg-white hover:bg-gray-100 '
+          ]"
+          href="#"
+          class="flex items-center justify-center px-4 h-10 leading-tight border border-gray-300"
+          @click.prevent="changePage(item)"
+          >{{ item }}</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700"
+          @click.prevent="nextPage"
+          :class="[currentPage === totalRecords ? 'cursor-not-allowed' : '']"
+          >下一頁</a
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted, computed, watch } from 'vue';
 import { initFlowbite } from 'flowbite';
+import Paginator from '@/components/Paginator.vue';
 
 // 呼叫api https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json
 // 取得資料後，將資料存入bikeinfo
@@ -92,35 +93,17 @@ const bikeinfoFilterBySort = computed(() => {
 
 // 當前頁數
 const currentPage = ref(1);
-const maxPage = 10;
 // 總頁數
-const bikeinfoFilterBySortTotal = computed(() => Math.ceil(bikeinfoFilterBySort.value.length / 10));
-
-// 計算當前顯示頁碼最多十頁
-const currentPageList = computed(() => {
-  let result = [];
-  // 取得當前頁碼的前五頁
-  const start = currentPage.value - 5 > 0 ? currentPage.value - 5 : 1;
-
-  // 取得當前頁碼的後五頁
-  const end =
-    start + maxPage - 1 <= bikeinfoFilterBySortTotal.value
-      ? start + maxPage - 1
-      : bikeinfoFilterBySortTotal.value;
-  for (let i = start; i <= end; i++) {
-    result.push(i);
-  }
-  return result;
-});
+const bikeinfoFilterBySortTotal = computed(() => bikeinfoFilterBySort.value.length);
 
 // 每頁顯示10筆資料
-const rows = 10;
+const rows = ref(20);
 const bikeinfoFilterBySortSliced = computed(() => {
   let result = [];
-  const start = (currentPage.value - 1) * rows;
+  const start = (currentPage.value - 1) * rows.value;
   const end =
-    start + rows <= bikeinfoFilterBySort.value.length
-      ? start + rows
+    start + rows.value <= bikeinfoFilterBySort.value.length
+      ? start + rows.value
       : bikeinfoFilterBySort.value.length;
   result = [...bikeinfoFilterBySort.value.slice(start, end)];
   return result;
@@ -128,26 +111,18 @@ const bikeinfoFilterBySortSliced = computed(() => {
 
 // 切換頁數
 const changePage = (page) => {
+  console.log('changePage', page);
   currentPage.value = page;
 };
 
-// 前一頁
-const prevPage = () => {
-  if (currentPage.value > 1) {
-    currentPage.value -= 1;
-  }
-};
-
-// 下一頁
-const nextPage = () => {
-  if (currentPage.value < bikeinfoFilterBySortTotal.value) {
-    currentPage.value += 1;
-  }
-};
-
 // 監聽搜尋關鍵字=>重置頁數
+const isReset = ref(false);
 watch(searchAr, () => {
-  currentPage.value = 1;
+  if (searchAr.value) {
+    isReset.value = true;
+  } else {
+    isReset.value = false;
+  }
 });
 
 onMounted(async () => {
@@ -346,41 +321,12 @@ onMounted(async () => {
     <!--datatable end-->
 
     <!--pagination start-->
-    <nav aria-label="Page navigation example" class="text-center mt-3">
-      <ul class="inline-flex -space-x-px text-base h-10">
-        <li>
-          <a
-            href="#"
-            class="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700"
-            @click="prevPage"
-            :class="[currentPage === 1 ? 'cursor-not-allowed' : '']"
-            >上一頁</a
-          >
-        </li>
-        <li v-for="(item, index) in currentPageList" :key="item">
-          <a
-            :class="[
-              currentPage === item
-                ? 'bg-blue-600 text-white hover:bg-blue-700'
-                : 'text-gray-500 bg-white hover:bg-gray-100 '
-            ]"
-            href="#"
-            class="flex items-center justify-center px-4 h-10 leading-tight border border-gray-300"
-            @click.prevent="changePage(item)"
-            >{{ item }}</a
-          >
-        </li>
-        <li>
-          <a
-            href="#"
-            class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700"
-            @click.prevent="nextPage"
-            :class="[currentPage === bikeinfoFilterBySortTotal ? 'cursor-not-allowed' : '']"
-            >下一頁</a
-          >
-        </li>
-      </ul>
-    </nav>
+    <Paginator
+      :totalRecords="bikeinfoFilterBySortTotal"
+      :rows="rows"
+      @page="changePage"
+      :isReset="isReset"
+    ></Paginator>
     <!--pagination end-->
   </div>
 </template>
